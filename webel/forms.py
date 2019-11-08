c9b07a16aa32ea30520143ca2b85382d051f51f3
omid <mhm.mot@gmail.com>
2019-11-08 11:32:39 +0330

    last

--- webel/forms.py
+++ webel/forms.py
@@ -11,3 +11,11 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 != password2:
+            raise forms.ValidationError("Your passwords do not match")
+        return password2            
+        
